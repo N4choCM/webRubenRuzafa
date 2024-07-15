1d0a76a7c188bd7dc05c611a5691a534a3857ea1
Juan Ignacio Campos Martí <104795448+N4choCM@users.noreply.github.com>
2024-07-15 23:32:10 +0200

    ⬆ UPDATED: Global State to handle Coach Screen state

--- src/state/AppProvider.jsx
+++ src/state/AppProvider.jsx
@@ -6,11 +6,13 @@ export const AppStateContext = createContext();
 export const initialState = {
   isDarkMode: false,
   isLoggedIn: false,
+  isCoachScreen: false,
 };
 
 export const Action = {
   TOGGLE_DARK_MODE: "TOGGLE_DARK_MODE",
   LOGIN: "LOGIN",
+  TOGGLE_COACH_SCREEN: "TOGGLE_COACH_SCREEN",
 };
 
 export const AppStateProvider = ({ children }) => {
